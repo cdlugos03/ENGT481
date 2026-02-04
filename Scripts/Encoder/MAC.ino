1d420e535e5d0ab600634f3f51cd1a68fcfc19f8
cdlugos03 <cdlugos@purdue.edu>
2026-02-03 20:24:47 -0500

    Added MAC.ino file

--- Scripts/Encoder/MAC.ino
+++ Scripts/Encoder/MAC.ino
@@ -1,1 +1,14 @@
+#include <WiFi.h>
 
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+
+  WiFi.mode(WIFI_STA);   // 🔴 REQUIRED
+  delay(100);
+
+  Serial.print("ESP32 STA MAC: ");
+  Serial.println(WiFi.macAddress());
+}
+
+void loop() {}
